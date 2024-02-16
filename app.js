d979c4192ca97164b2df28e1349030fc56c3f754
Nair, Anuj <anujnairr@gmail.com>
2024-02-16 16:54:34 +0530

    Swtiched from pug to ejs engine

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
-// const productsRoutes = require('./routes/');
+const errorController = require('./controllers/error');
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const errorController = require('./controllers/error');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -19,6 +19,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use(errorController.errorPage);
+app.use(errorController.get404);
 
 app.listen(3000);
