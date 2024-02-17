ff73c0c2e73d55f04ec9c820630632d0cf6ffc7d
Nair, Anuj <anujnairr@gmail.com>
2024-02-17 18:33:29 +0530

    Before introducing database

---
+++ models/cart.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+const path = require('path')
+
+const p = path.join(
+    path.dirname(process.mainModule.filename),
+    'data',
+    'cart.json');
+
+module.exports = class Cart {
+    static addProduct() {
+        fs.readFile(p, (err, fileContent) => {
+            let cart = { products: [], totalPrice: 0 };
+            if (!err) {
+                cart = JSON.parse(fileContent);
+            }
+        })
+    }
+}
