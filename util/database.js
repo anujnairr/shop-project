708a69525a467f2f10b864895f5ba5644ef145a0
Nair, Anuj <anujnairr@gmail.com>
2024-02-21 17:54:43 +0530

    new model with sequelize created.

--- util/database.js
+++ util/database.js
@@ -1,18 +1,8 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('node-complete', 'root', 'Makayiram_01', {
-    dialet: 'mysql', host: 'localhost'
+
+const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
+    dialect: 'mysql',
+    host: 'localhost'
 });
 
 module.exports = sequelize;
-
-
-// const mysql = require('mysql2')
-
-// const pool = mysql.createPool({
-//     host: 'localhost',
-//     user: 'root',
-//     database: 'node-complete',
-//     password: 'Makayiram_01'
-// });
-
-// module.exports = pool.promise();
