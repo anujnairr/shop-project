aac9ec6595d8cf47be2cace0a2fabdeae173f7e4
Nair, Anuj <anujnairr@gmail.com>
2024-02-28 17:45:57 +0530

    User Model and Products association created.

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 const app = express();
 
@@ -13,7 +15,6 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const { Sequelize } = require('sequelize');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -23,9 +24,14 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-sequelize.sync().then(result => {
-    app.listen(3000);
-})
+Product.belongsTo(User, { constaints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+
+sequelize
+    .sync({ force: true })
+    .then(result => {
+        app.listen(3000);
+    })
     .catch(err => {
         console.log(err);
     })
